d12915ec5cb30af5118f54416e007788ea113f00
Simon <grandins@student.chalmers.se>
2026-03-21 14:54:25 +0100

    Made the Breadth-First-Search algoritm

--- src/graphs/Breadth-first-search.ts
+++ src/graphs/Breadth-first-search.ts
@@ -4,6 +4,8 @@ import { Graph } from "~/graph";
 import { updateDefault } from "~/helpers";
 import { WeightedConnection } from "~/objects/weigted-connection";
 import { WeightedGraphNode } from "~/objects/weightedgraph-node";
+import Queue from "~/basic/Queue"
+import { HighlightCircle } from "~/objects/highlight-circle";
 
 export const BreadthMessages = {
     //if you want to change messages that already exist in
@@ -23,11 +25,35 @@ export class Breadth extends BaseGraph implements Graph {
             await this.pause("error.nullGraph")
             return
         }
+        this.graph.setHighlight(false)
+        const result = this.breadthSearch()
+        await this.nodeTraversalVisualisation(result)
+        this.graph.setHighlight(false)
     }
 
     breadthSearch(): WeightedConnection<WeightedGraphNode>[] {
-        const visitOrder: WeightedConnection<WeightedGraphNode>[] = [];
+        const visitOrder: Queue<WeightedGraphNode> = new Queue();
+        const edges: WeightedConnection<WeightedGraphNode>[] = [];
+        const visitedNodes: WeightedGraphNode[] = []
+        const toVisit: WeightedGraphNode[] = []
 
-        return []
+        visitOrder.enqueue(this.graph!)
+
+        while(visitOrder.size() > 0) {
+            const currNode  = visitOrder.dequeue()
+            const currEdges = currNode?.$outgoing
+            visitedNodes.push(currNode!)
+
+            if (!currEdges) continue
+
+            for (const k of Object.values(currEdges)) {
+                if (toVisit.includes(k!.$end) || visitedNodes.includes(k!.$end)) 
+                    continue
+                visitOrder.enqueue(k!.$end)
+                edges[edges.length] = k!
+                toVisit.push(k!.$end)
+            }
+        }
+        return edges
     }
 }
